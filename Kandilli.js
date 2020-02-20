8f46d2bd8f8ebc963b3517f10acc8bb13a67c9c2
altunah <34680612+altunah@users.noreply.github.com>
2020-02-21 01:26:58 +0300

    Create Kandilli.js
    Browser'da F12'ye basın, Console Tab'ına geçin ve altta gelen editöre bu kodu yapıştırıp enter'a basın.

---
+++ Kandilli.js
@@ -0,0 +1,22 @@
+var elem = document.getElementsByTagName('pre')[0];
+var text = elem.innerText;
+var index = /\d\d\d\d\.\d\d*/.exec(text).index;
+var introduction = text.substring(0, index);
+text = text.substring(index);
+var lines = text.match(/[^\r\n]+/g);
+var lineObjArr = [];
+for (var i = 0; i < lines.length; i++)
+{
+	var lineObj = {};
+	lineObj.Magnitude = parseFloat(lines[i].substring(60, 62));
+	lineObj.Text = lines[i];
+	lineObjArr.push(lineObj);
+}
+lineObjArr.sort(function(a, b){return b.Magnitude - a.Magnitude;});
+var sortedText = '';
+for (var i = 0; i < lineObjArr.length; i++)
+{
+	sortedText += lineObjArr[i].Text + '\r\n';
+}
+elem.innerText = introduction + sortedText;
+
